c285e0edac40436bd717540044590f3052158823
nishanthspk <spknishanth2004@gmail.com>
2024-11-27 21:34:51 +0530

    Balance Updation

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,34 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { LuSettings } from "react-icons/lu";
 import { IoMdArrowDown } from "react-icons/io";
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-    getDefaultConfig,
-    RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import {
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-} from 'wagmi/chains';
 import { CustomButton } from './CustomButton';
 import SellSection from './SellSection';
 import BuySection from './BuySection';
 import SellSearchBar from './SellSearchBar'; 
 import BuySearchBar from './BuySearchBar';   
 import axios from "axios";
+import Web3 from 'web3';
+import { useAccount } from 'wagmi';
 
-const config = getDefaultConfig({
-    appName: 'My RainbowKit App',
-    projectId: 'YOUR_PROJECT_ID',
-    chains: [mainnet, polygon, optimism, arbitrum, base],
-    ssr: true,
-});
+
+const web3 = new Web3(window.ethereum || 'https://mainnet.infura.io/v3/5ff618058593414aae2f28055c712825');
 
 const Market = () => {
     const [sellInputValue, setSellInputValue] = useState('');
@@ -41,6 +24,130 @@ const Market = () => {
     const [loading, setLoading] = useState(false)
     const [autoShowSecondBuy, setAutoShowSecondBuy] = useState(false);
     const [autoShowSecondSell, setAutoShowSecondSell] = useState(false);
+    const storedToken = localStorage.getItem("sellSelectedToken");
+    const TokenJsonData = JSON.parse(storedToken);
+    const [balance, setBalance] = useState();
+    const { address: accountAddress } = useAccount();
+
+    // const account = useAccount();
+    
+    const tokenABI = [
+      {
+          "constant": true,
+          "inputs": [
+              {
+                  "name": "_owner",
+                  "type": "address"
+              },
+              {
+                  "name": "_spender",
+                  "type": "address"
+              }
+          ],
+          "name": "allowance",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint256"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [{ "name": "_owner", "type": "address" }],
+          "name": "balanceOf",
+          "outputs": [{ "name": "", "type": "uint256" }],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      },
+      {
+          "constant": false,
+          "inputs": [
+              {
+                  "name": "_spender",
+                  "type": "address"
+              },
+              {
+                  "name": "_value",
+                  "type": "uint256"
+              }
+          ],
+          "name": "approve",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "bool"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "nonpayable",
+          "type": "function"
+      },
+      {
+          "constant": true,
+          "inputs": [],
+          "name": "decimals",
+          "outputs": [
+              {
+                  "name": "",
+                  "type": "uint8"
+              }
+          ],
+          "payable": false,
+          "stateMutability": "view",
+          "type": "function"
+      }
+  ];
+
+  const getTokenBalance = async (walletAddress, tokenAddress) => {
+    try {
+
+        if (tokenAddress === '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE') {
+            const balance = await web3.eth.getBalance(walletAddress);
+            const formattedBalance = parseFloat(web3.utils.fromWei(balance, 'ether'));
+            console.log("Balance (in Ether):", formattedBalance);
+            return formattedBalance;
+        } else {
+            console.log("Token Address", tokenAddress);
+            const tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
+            // Fetch token decimals
+            const decimals = await tokenContract.methods.decimals().call();
+            console.log(`Decimals: ${decimals}`);
+            // Fetch balance
+            const rawBalance = await tokenContract.methods.balanceOf(walletAddress).call();
+            console.log(`rawBalance: ${rawBalance}`);
+            console.log(`Balance: ${rawBalance}`);
+            return rawBalance;
+        }
+    } catch (error) {
+        console.error("Error fetching token balance:", error);
+    }
+};
+
+  const fetchingBalance = async () => {
+    try {
+      if (accountAddress) { // Ensure accountAddress is available
+        const result = await getTokenBalance(accountAddress, TokenJsonData?.address);
+        setBalance(result);
+        return result;
+      } else {
+        console.error("No wallet connected.");
+      }
+    } catch (error) {
+      console.error("Error fetching balance", error);
+    }
+
+}
+
+useEffect(() => {
+  if (TokenJsonData && accountAddress) {
+    fetchingBalance();
+  }
+}, [TokenJsonData, accountAddress]);
 
     const [error, setError] = useState(false)
     const [buyToken, setBuyToken] = useState(() => {
@@ -108,7 +215,7 @@ const Market = () => {
         }
     };
 
-    const queryClient = new QueryClient();
+   
 const fetchSwapQuote = async (buyToken, sellToken, chainId = 8453) => {
   if (!buyToken || !sellToken) {
     console.error("Tokens are not selected.");
@@ -173,9 +280,7 @@ const fetchSwapQuote = async (buyToken, sellToken, chainId = 8453) => {
     
 
     return (
-      <WagmiProvider config={config}>
-        <QueryClientProvider client={queryClient}>
-          <RainbowKitProvider>
+      
             <div className="flex justify-center items-center h-screen bg-black p-4">
               <div className="bg-white h-[350px] border rounded-[1.625rem] w-full sm:h-[500px] sm:w-[500px]">
                 <div className="flex justify-between pl-[1.25rem] py-1 pr-[0.75rem] border-b items-center sm:py-3">
@@ -290,14 +395,16 @@ const fetchSwapQuote = async (buyToken, sellToken, chainId = 8453) => {
                   <div className="relative w-full h-[1px] bg-[#f1f2f4]"></div>
 
                   <div className="px-[1.5rem] py-[1rem] justify-center flex items-center">
-                    <CustomButton />
-                  </div>
+                        {balance < parseFloat(sellInputValue || 0) ? (
+                            'Not Enough Balance'
+                        ) : (
+                            <CustomButton />
+                        )}
+                    </div>
                 </div>
               </div>
             </div>
-          </RainbowKitProvider>
-        </QueryClientProvider>
-      </WagmiProvider>
+         
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,40 @@ import App from './App.jsx'
 import { Buffer } from 'buffer';
 window.Buffer = Buffer;
 
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+    getDefaultConfig,
+    RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { useAccount, WagmiProvider } from 'wagmi';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import {
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+} from 'wagmi/chains';
+
+const config = getDefaultConfig({
+    appName: 'My RainbowKit App',
+    projectId: 'YOUR_PROJECT_ID',
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    ssr: true,
+});
+
+
+const queryClient = new QueryClient();
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <App />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
 )
